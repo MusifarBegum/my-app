24f70ed7f78f05f51be44c9464c7402dc44f1199
musifar.begum <musifar.begum@gmail.com>
2024-10-07 18:53:45 +0530

    updated video height

--- src/Components/WatchPage.js
+++ src/Components/WatchPage.js
@@ -16,7 +16,7 @@ const WatchPage = () => {
     <div className='flex flex-col w-full'>
       <div className="px-5 flex">
         <div className="p-14 ">
-        <iframe width="1000px" height="500px"  src={"https://www.youtube.com/embed/" +searchParams.get("v")} 
+        <iframe width="900px" height="500px"  src={"https://www.youtube.com/embed/" +searchParams.get("v")} 
         title="YouTube video player" frameBorder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
         referrerPolicy="strict-origin-when-cross-origin" 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,6 @@ export const SEARCH_SUGGESTIONS_API ="http://suggestqueries.google.com/complete/
 export const COMMENTSTHREAD = "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId="
 export const COMMENTSKEY ="&key="+GOOGLE_API_KEY
 export const YOUTUBE_LIKEDVIDEOS_API =" https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&myRating=like&key="+GOOGLE_API_KEY;
-export const LIST_OF_BUTTONS=["All","Live","Comic Books","Resorts","Gaming","Travel","kids","Music","Dance","Sports","Watched","For you"]
+export const LIST_OF_BUTTONS=["All","Live","Comic","Resorts","Gaming","Travel","kids","Music","Dance","Sports","Watched"]
 export const VIDEO_ID_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id="
 export const VIDEO_ID_KEY ="&key="+GOOGLE_API_KEY;
